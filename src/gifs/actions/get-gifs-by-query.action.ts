500fc455a6af4a4caf131aea43f894fbfffb5eec
Gonzalo Muñoz <gmunoz@knowmadmood.com>
2026-05-05 15:58:05 +0200

    feat(api): implement http requests and data interfaces for giphy

---
+++ src/gifs/actions/get-gifs-by-query.action.ts
@@ -0,0 +1,20 @@
+import type { Gif } from "../interfaces/gif.interface";
+import type { GiphyResponse } from "../interfaces/giphy.response";
+import { giphyApi } from "../api/giphy.api";
+
+export const getFigsByQuery = async (query: string): Promise<Gif[]> => {
+  const response = await giphyApi<GiphyResponse>("/search", {
+    params: {
+      q: query,
+      limit: 10,
+    },
+  });
+
+  return response.data.data.map((gif) => ({
+    id: gif.id,
+    title: gif.title,
+    url: gif.images.fixed_height.url,
+    width: Number(gif.images.fixed_height.width),
+    height: Number(gif.images.fixed_height.height),
+  }));
+};
