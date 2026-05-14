afd8e01161c2e9694f8d6d9652bf716424536015
Gonzalo Muñoz <gmunoz@knowmadmood.com>
2026-05-14 17:44:28 +0200

    test: add unit tests for useGifs custom hook functionality

--- src/gifs/hooks/useGifs.test.tsx
+++ src/gifs/hooks/useGifs.test.tsx
@@ -1,6 +1,7 @@
-import { describe, expect, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import { useGifs } from "./useGifs";
 import { act, renderHook } from "@testing-library/react";
+import * as gifActions from "../actions/get-gifs-by-query.action";
 
 describe("useGifs", () => {
   test("should return default values and methods", () => {
@@ -28,5 +29,48 @@ describe("useGifs", () => {
     });
 
     expect(result.current.gifs.length).toBe(10);
-  })
+  });
+
+  test("should return a list of gifs from cache", async () => {
+    const { result } = renderHook(() => useGifs());
+
+    await act(async () => {
+      await result.current.handleTermClicked("react");
+    });
+
+    expect(result.current.gifs.length).toBe(10);
+
+    vi.spyOn(gifActions, "getGifsByQuery").mockRejectedValue(
+      new Error("This is my custom error"),
+    );
+
+    await act(async () => {
+      await result.current.handleTermClicked("react");
+    });
+
+    expect(result.current.gifs.length).toBe(10);
+  });
+
+  test("should return no more than 8 previous terms", async () => {
+    const { result } = renderHook(() => useGifs());
+
+    vi.spyOn(gifActions, "getGifsByQuery").mockResolvedValue([]);
+
+    await act(async () => {
+      await result.current.handleSearch("1");
+      await result.current.handleSearch("2");
+      await result.current.handleSearch("3");
+      await result.current.handleSearch("4");
+      await result.current.handleSearch("5");
+      await result.current.handleSearch("6");
+      await result.current.handleSearch("7");
+      await result.current.handleSearch("8");
+      await result.current.handleSearch("9");
+    });
+
+    expect(result.current.previousTerms.length).toBe(8);
+    expect(result.current.previousTerms).toEqual(
+      expect.arrayContaining(["9", "8", "7", "6", "5", "4", "3", "2"]),
+    );
+  });
 });
